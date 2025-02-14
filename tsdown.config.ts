d537ffe0ffb6e464dd169658789497e81d3db74a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-14 15:43:49 +0800

    refactor: migrate to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src'],
   format: ['cjs', 'esm'],
-  target: 'node14',
   clean: true,
   dts: true,
 })
